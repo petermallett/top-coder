274e870955957ab2d1a020ac8ae15cfe637b1392
Peter Mallett <peter.mallett@gmail.com>
2011-05-27 19:06:11 -0400

    incremental work on mafiagame.

--- mafiagame/mafiagame.py
+++ mafiagame/mafiagame.py
@@ -1,3 +1,5 @@
+import random
+
 games = [
 	[3, [1, 1, 1], 1.0],
 	[5, [1, 2, 3], 0.0],
@@ -5,13 +7,54 @@ games = [
 	[23, [17, 10, 3, 14, 22, 5, 11, 10, 22, 3, 14, 5, 11, 17], 0.14285714285714285]
 ]
 
+def vote_lowest(players, opinions):
+	if (len(players) != len(opinions)):
+		nonvoter_count = len(players) - len(opinions)
+		for x in range(0, nonvoter_count):
+			min_votes = min(votes)
+			min_count = votes.count(min_votes)
+			if (min_count == 1):
+				index = votes.index(min_votes)
+			else:
+				# choose one of the players with the min votes to vote for
+				min_indexes = []
+				for i, v in enumerate(votes):
+					if (v == min_votes):
+						min_indexes.append(i)
+					if (len(min_indexes) == min_count):
+						break
+				index = random.randrange(0, len(min_indexes))
+			
+			# vote for the selected player
+			votes[index] += 1
+
+# search the current votes for a losing player
+# if there is a loser, return the index
+# if not return 0 and update vulnerable
+def check_state(votes, vulnerable):
+	max_votes = max(votes)
+	if (votes.count(max_votes) == 1):
+		return votes.index(max_votes)
+	else:
+		for i, v in enumerate(votes):
+			if (v == max_votes):
+				vulnerable[i] = 1
+			else:
+				vulnerable[i] = 0
+		return 0
+
 def play(players, opinions):
 	vulnerable = []
 
 	# first vote
 	for x in opinions:
 		players[x]['votes'] += 1
-	
+	# complete first vote
+	vote_lowest(players, opinions)
+
+	result = check_state(players, vulnerable)
+	print(result)
+
 def probabilityToLose(playerCount, opinions):
 	# initialize players
 	players = []
@@ -26,4 +69,4 @@ for x in games:
 	expected = x[2]
 	prob = probabilityToLose(player_count, opinions)
 	if (prob != expected):
-		print('error')
+		print('error: ', prob, ' should be ', expected)
